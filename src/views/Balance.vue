017273ae02af1a9f28c56d74217f4befd6ba65ff
Raúl Cubitto <raulcubitto@gmail.com>
2019-01-02 13:10:57 -0300

    Revert compact tables

--- src/views/Balance.vue
+++ src/views/Balance.vue
@@ -5,13 +5,13 @@
         <table class="w-full">
           <thead>
             <tr class="bg-red-lightest border-b-4 border-red-lighter uppercase text-red-light tracking-wide text-xs font-bold">
-              <th class="text-left p-2">
+              <th class="text-left p-4">
                 Description → [Monthly]
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 U$S
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 AR$
               </th>
             </tr>
@@ -23,24 +23,24 @@
               class="text-grey-darkest"
               :class="{ 'bg-grey-lighter': index % 2 === 1 }"
             >
-              <td class="p-2">
+              <td class="p-4">
                 {{ entry.detail }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "USD" ? format(entry.price).get() : format(entry.price).toUSD(exchange) }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "ARS" ? format(entry.price).get() : format(entry.price).toARS(exchange) }}
               </td>
             </tr>
             <tr class="text-grey-darkest font-bold bg-grey-light">
-              <td class="text-left p-2">
+              <td class="text-left p-4">
                 Total
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("outcome", "monthly", "USD")).get() }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("outcome", "monthly", "ARS")).get() }}
               </td>
             </tr>
@@ -53,13 +53,13 @@
         <table class="w-full">
           <thead>
             <tr class="bg-red-lightest border-b-4 border-red-lighter uppercase text-red-light tracking-wide text-xs font-bold">
-              <th class="text-left p-2">
+              <th class="text-left p-4">
                 Description → [Yearly]
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 U$S
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 AR$
               </th>
             </tr>
@@ -71,24 +71,24 @@
               class="text-grey-darkest"
               :class="{ 'bg-grey-lighter': index % 2 === 1 }"
             >
-              <td class="p-2">
+              <td class="p-4">
                 {{ entry.detail }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "USD" ? format(entry.price).get() : format(entry.price).toUSD(exchange) }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "ARS" ? format(entry.price) : format(entry.price).toARS(exchange) }}
               </td>
             </tr>
             <tr class="text-grey-darkest font-bold bg-grey-light">
-              <td class="text-left p-2">
+              <td class="text-left p-4">
                 Total
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("outcome", "yearly", "USD")).get() }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("outcome", "yearly", "ARS")).get() }}
               </td>
             </tr>
@@ -99,13 +99,13 @@
         <table class="w-full">
           <thead>
             <tr class="bg-green-lightest border-b-4 border-green-light uppercase text-green-dark tracking-wide text-xs font-bold">
-              <th class="text-left p-2">
+              <th class="text-left p-4">
                 Description → [Yearly]
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 U$S
               </th>
-              <th class="text-right p-2">
+              <th class="text-right p-4">
                 AR$
               </th>
             </tr>
@@ -117,24 +117,24 @@
               class="text-grey-darkest"
               :class="{ 'bg-grey-lighter': index % 2 === 1 }"
             >
-              <td class="p-2">
+              <td class="p-4">
                 {{ entry.detail }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "USD" ? format(entry.price).get() : format(entry.price).toUSD(exchange) }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ entry.currency === "ARS" ? format(entry.price).get() : format(entry.price).toARS(exchange) }}
               </td>
             </tr>
             <tr class="text-grey-darkest font-bold bg-grey-light">
-              <td class="text-left p-2">
+              <td class="text-left p-4">
                 Total
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("income", "yearly", "USD")).get() }}
               </td>
-              <td class="text-right p-2">
+              <td class="text-right p-4">
                 {{ format(entriesByRangeAndCurrencySum("income", "yearly", "ARS")).get() }}
               </td>
             </tr>
