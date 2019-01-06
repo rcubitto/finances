ec306cebb3f02d259f918ad08e3cca3ecb5c93ad
Raúl Cubitto <raulcubitto@gmail.com>
2019-01-06 01:29:51 -0300

    Refactor PIll component; Add horizontal line component

---
+++ src/components/DiscountStep.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col justify-center items-center flex-1">
+    <p class="text-grey-darker font-bold -mt-4">
+      {{ label || "" }}
+    </p>
+    <div class="border-b-4 w-full border-grey" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
--- src/components/Pill.vue
+++ src/components/Pill.vue
@@ -1,25 +1,39 @@
 <template>
   <div :class="css">
     <slot>
-      <p
-        v-if="title"
-        class="text-2xl"
-        :class="`text-${color}-lightest`"
-      >
-        {{ title }}
-      </p>
-      <span
-        v-if="subtitle"
-        :class="`text-${color}-lighter`"
-      >
-        {{ subtitle }}
-      </span>
+      <div class="flex justify-between items-center">
+        <div
+          v-if="title || subtitle"
+          class="flex flex-col"
+        >
+          <p
+            v-if="title"
+            class="text-2xl"
+            :class="`text-${color}-lightest`"
+          >
+            {{ title }}
+          </p>
+          <span
+            v-if="subtitle"
+            :class="`text-${color}-lighter`"
+          >
+            {{ subtitle }}
+          </span>
+        </div>
+        <DollarIcon
+          :circle-color="`text-${color}-dark`"
+          :sign-color="`text-${color}-lighter`"
+          size="10"
+        />
+      </div>
     </slot>
   </div>
 </template>
 
 <script>
+import DollarIcon from "@/components/DollarIcon";
 export default {
+  components: { DollarIcon },
   props: {
     color: {
       type: String,
@@ -41,7 +55,7 @@ export default {
   computed: {
     css() {
       return (
-        `bg-${this.color} rounded p-6 overflow-hidden shadow-md` +
+        `bg-${this.color} rounded p-6 overflow-hidden shadow-md flex-1` +
         (this.extraCss ? ` ${this.extraCss}` : "")
       );
     }
