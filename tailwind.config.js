c06f674cf0374ac003e2a261df29b7264927d22b
Raúl Cubitto <raulcubitto@gmail.com>
2020-03-09 04:21:55 -0300

    Configure tailwind spinner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
+  theme: {
+    spinner: theme => ({
+      default: {
+        color: theme("colors.white"),
+        size: "1em",
+        border: "2px",
+        speed: "500ms"
+      }
+    })
+  },
   variants: {},
-  plugins: []
-}
+  plugins: [
+    require("tailwindcss-spinner")() // no options to configure
+  ]
+};
